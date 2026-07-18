1d0502714b7f904c76ea1b6970b6bc2d6fca9070
LukasChemieBT13 <lukas45.seitle@t-online.de>
2026-07-18 16:20:24 +0200

    search function

--- popup.js
+++ popup.js
@@ -1,17 +1,152 @@
 console.log("Popup geladen");
 
 
-document.getElementById("saveTabs")
-.addEventListener("click", () => {
+
+const saveButton = document.getElementById("saveTabs");
+
+const tabList = document.getElementById("tabList");
+
+const searchInput = document.getElementById("search");
+let allTabs = [];
+
+
+// Tabs speichern
+
+saveButton.addEventListener("click", () => {
 
 
     chrome.runtime.sendMessage({
+
         action: "saveTabs"
+
     });
 
 
     document.getElementById("status").innerHTML =
-    "Tabs werden gespeichert...";
+    "Tabs gespeichert";
+
+
+    loadTabs();
+
+});
+
+
+
+// Tabs laden
+
+function loadTabs(){
+
+
+    chrome.storage.local.get(
+        ["savedTabs"],
+
+        (result)=>{
+
+
+            const tabs = result.savedTabs || [];
+
+
+allTabs = tabs;
+
+
+displayTabs(tabs);
+
+
+        }
+
+    );
+
+
+}
+
+
+
+// Tabs anzeigen
+
+function displayTabs(tabs){
+
+
+    tabList.innerHTML = "";
+
+
+    tabs.forEach(tab => {
+
+
+        const card = document.createElement("div");
+
+
+        card.className = "tab-card";
+
+
+        card.innerHTML = `
+
+    <strong>▶ ${tab.title}</strong>
+
+    <br>
+
+    <small>${tab.domain}</small>
+
+`;
+
+
+        card.addEventListener("click", () => {
+
+    chrome.tabs.create({
+
+        url: tab.url
+
+    });
+
+});
+
+
+tabList.appendChild(card);
+
+
+    });
+
+
+}
+
+searchInput.addEventListener("input", () => {
+
+
+    const searchTerm = searchInput.value.toLowerCase();
+
+
+
+    const filteredTabs = allTabs.filter(tab => {
+
+
+        return (
+
+            tab.title.toLowerCase().includes(searchTerm)
+
+            ||
+
+            tab.url.toLowerCase().includes(searchTerm)
+
+            ||
+
+            tab.domain.toLowerCase().includes(searchTerm)
+
+            ||
+
+            tab.category.toLowerCase().includes(searchTerm)
+
+        );
+
+
+    });
+
+
+
+    displayTabs(filteredTabs);
 
 
 });
+
+
+// Beim Öffnen laden
+
+loadTabs();
